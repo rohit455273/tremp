f4296805f1c4a713f808316cf40a17cebb8ef5a6
rohit <rohitkwankhede@gmail.com>
2022-11-25 07:58:19 +0530

    ll

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -2,10 +2,8 @@ context("app")
 
 testServer(expr = {
   # Set the `size` slider and check the output
-  session$setInputs(size = 6)
-  expect_equal(output$sequence, "1 2 3 4 5 6")
+  session$setInputs(x = 1,y=1,z=1)
+  expect_equal(output$out, 0)
 
-  session$setInputs(size = 12)
-  expect_equal(output$sequence, "1 10 11 12 2 3 4 5 6 7 8 9")
-
+  
 })
