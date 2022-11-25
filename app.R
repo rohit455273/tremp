2e0cc597b7a6b3f41e88b798f948e4fd89f2fd9b
rohit <rohitkwankhede@gmail.com>
2022-11-25 07:53:30 +0530

    app

--- app.R
+++ app.R
@@ -1,35 +1,15 @@
-ui <- fluidPage(
-  # ======== Modules ========
-  # exampleModuleUI is defined in R/example-module.R
-  wellPanel(
-    h2("Modules example"),
-    exampleModuleUI("examplemodule1", "Click counter #1"),
-    exampleModuleUI("examplemodule2", "Click counter #2")
-  ),
-  # =========================
-
-  wellPanel(
-    h2("Sorting example"),
-    sliderInput("size", "Data size", min = 5, max = 20, value = 10),
-    div("Lexically sorted sequence:"),
-    verbatimTextOutput("sequence")
+library(shiny)
+  ui <- fluidPage(
+    numericInput("x", "x", 0),
+    numericInput("y", "y", 1),
+    numericInput("z", "z", 2),
+    textOutput("out")
   )
-)
-
-server <- function(input, output, session) {
-  # ======== Modules ========
-  # exampleModuleServer is defined in R/example-module.R
-  exampleModuleServer("examplemodule1")
-  exampleModuleServer("examplemodule2")
-  # =========================
-
-  data <- reactive({
-    # lexical_sort from R/example.R
-    lexical_sort(seq_len(input$size))
-  })
-  output$sequence <- renderText({
-    paste(data(), collapse = " ")
-  })
-}
-
-shinyApp(ui, server)
+  server <- function(input, output, session) {
+    xy <- reactive(input$x - input$y)
+    yz <- reactive(input$z + input$y)
+    xyz <- reactive(xy() * yz())
+    output$out <- renderText(paste0("Result: ", xyz()))
+  }
+  
+  shinyApp(ui,server)
